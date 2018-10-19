7b1d04424cb2d6f7cc27f8f0e6bc48674e8123e4
Albert Autin <albert.autin@thetradedesk.com>
2018-10-19 10:49:55 -0500

    i guess runact should accept params

--- ala/runact.sh
+++ ala/runact.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+upto=$1
+downto=$2
+if [ -z $upto ]
+then
+    upto=120
+fi
+
+if [ -z $downto ]
+then
+    downto=10
+fi
+
+echo "testing ${upto}X - ${downto}X configs"
+exit
 while `pidof actprep>/dev/null`; do echo "waiting for actprep: $(ps aux|grep -v grep|grep actprep)";sleep 60;done;sleep 60
 for n in {12..1}; do
   let x=n*10;
