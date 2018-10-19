a3f6dfbfd7abd653f59bc5e6672dfa444eea353d
Albert Autin <albert.autin@thetradedesk.com>
2018-10-19 14:24:25 -0500

    moved the binary

--- ala/run_act_storage.sh
+++ ala/run_act_storage.sh
@@ -16,6 +16,6 @@ echo "testing ${upto}X - ${downto}X configs"
 while `pidof actprep>/dev/null`; do echo "waiting for actprep: $(ps aux|grep -v grep|grep actprep)";sleep 60;done;sleep 60
 for n in {12..1}; do
   let x=n*10;
-  /opt/act/act /opt/actconfig_${x}x.txt > actconfig_${x}x.txt.out;
+  /opt/act/act/target/bin/act_storage /opt/actconfig_${x}x.txt > actconfig_${x}x.txt.out;
   sleep 120;
 done
